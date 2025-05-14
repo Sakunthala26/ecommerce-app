275573af75f14754f26feb697baffffcac76926c
Sakunthala26 <kumarisakunthala26@gmail.com>
2025-05-14 20:44:31 +0530

    search bar update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,8 +4,7 @@ import LatestCollection from '../components/LatestCollection';
 import BestSeller from '../components/BestSeller';
 import OurPolicy from '../components/OurPolicy';
 import NewsLetterBox from '../components/NewsLetterBox';
-import Collection from "../pages/Collection";
-
+import Collection from "./Collection";
 
 const Home = () => {
   return (
