211b066a3ab55054e7e5db44d245c0adfa21f728
Sakunthala26 <kumarisakunthala26@gmail.com>
2025-05-18 17:40:05 +0530

    one product click to show

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { ShopContext } from '../context/ShopContext';
+import { assets } from '../assets/frontend_assets/assets';
 
 const Product = () => {
   const { productId } = useParams();
@@ -24,20 +25,37 @@ const Product = () => {
 
   return productData ?  (
     <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
-      {/*Product Data */}
+      {/* --------- Product Data --------- */}
       <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
 
-        {/* Product Images */}
+        {/* --------- Product Images --------- */}
         <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
           <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
             {
               productData.image.map((item,index)=>(
-                <img src={item}  key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' alt="" />
+                <img onClick={()=>setImage(item)} src={item}  key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' alt="" />
               ))
             }
             
           </div>
+         <div className='w-full sm:w-[80%]'>
+              <img className='w-full h-auto' src={image} alt="" />
+         </div>
+        </div>
 
+        {/* --------- Product Info --------- */}
+        <div className='flex-1'>
+           <h1 className='font-medium text-2xl mt-2'>{productData.name}</h1>
+           <div className='flex items-center gap-1 mt-2'>
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_dull_icon} alt="" className="w-3 5" />
+            <p className='pl-2'>(122)</p>
+
+           </div>
+           <p></p>
         </div>
       </div>
     </div>
