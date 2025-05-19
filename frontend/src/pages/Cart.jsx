920fa4b480761e85b054f972f03ac1d83f4bed75
Sakunthala26 <kumarisakunthala26@gmail.com>
2025-05-19 19:25:35 +0530

    CartTotal add

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -2,10 +2,12 @@ import React, { useContext, useEffect, useState } from 'react'
 import Product from './Product';
 import { ShopContext } from '../context/ShopContext';
 import Title from '../components/Title';
+import { assets } from '../assets/frontend_assets/assets';
+import CartTotal from '../components/CartTotal';
 
 const Cart = () => {
 
-  const {products, currency, cartItems } = useContext(ShopContext);
+  const {products, currency, cartItems,updateQuantity } = useContext(ShopContext);
 
   const [cartData,setCartData] = useState([]);
 
@@ -52,11 +54,20 @@ const Cart = () => {
                   </div>
 
                 </div>
-                  <input className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' type="number" min={1} defaultValue={item.quantity} />
+                  <input onChange={(e)=> e.target.value === '' || e.target.value === '0' ? null : updateQuantity(item._id,item.size,Number(e.target.value))} className='border max-w-8 sm:max-w-12 px-1 sm:px-2 py-1' type="number" min={1} defaultValue={item.quantity} />
+                  <img onClick={()=>updateQuantity(item._id,item.size,0)} className='w-4 mr-4 sm:w-4 cursor-pointer' src={assets.bin_icon} alt="" />
                </div>
             )
           })
         }
+      </div>
+
+      <div className='flex justify-end my-20'>
+        <div  className='w-full sm:w-[450px]'>
+          <CartTotal/>
+
+        </div>
+
       </div>
       
     </div>
