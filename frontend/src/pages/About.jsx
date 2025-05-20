91f56f758430de47530b4d8200bbc3c42be1bfc4
Sakunthala26 <kumarisakunthala26@gmail.com>
2025-05-20 23:48:30 +0530

    To Update

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
         <Title text1={'ABOUT'} text2={'US'}/>
       </div>
       <div className='my-10 flex flex-col md:flex-row gap-16'>
-         <img className='w-full md:max-w-[450px]' src={assets.about_img} alt="" />
+         <img className='w-full md:max-w-[400px]' src={assets.about_img} alt="" />
          <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
           <p>Forever was born out of a passion for innovation and a desire to revolutionize the way people shop online.Our journey began with a simple idea: to provide a platform where customers can easily discover ,explore ,and purchase a wide range of  products from the comfortg their homes.</p>
           <p>Since our inception, we've worked tirelessy to curate a deverse selection of high-quality products that cater to every taste and preference.</p>
