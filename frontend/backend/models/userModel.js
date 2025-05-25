c20969f323d9eec51f9304a4036b434f5759c6e0
Sakunthala26 <kumarisakunthala26@gmail.com>
2025-05-25 20:30:04 +0530

    mongoose db

--- frontend/backend/models/userModel.js
+++ frontend/backend/models/userModel.js
@@ -3,5 +3,5 @@ const userSchema = new mongoose.Schema({
     name: { type : String, required: true},
     email: { type : String, required: true, unique: true},
     password: { type : String, required: true},
-    cartData: { type: Object, default: {}}
+    cartData: { type: Object, default: {}},
 })
