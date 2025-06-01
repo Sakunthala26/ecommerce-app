633552fc482c7baaa3f2dab6122f126e5f7e5340
Sakunthala26 <kumarisakunthala26@gmail.com>
2025-06-01 23:31:38 +0530

    UPDATE

---
+++ frontend/backend/middeleware/adminAuth.js
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken'
+
+const adminAuth = async (req,res,next) => {
+    try {
+        const { token } = req.headers
+        if (!token) {
+            return res.json({success:false,message:"Not Authorized Login Again"})
+        }
+        const token_decode = jwt.verify(token,process.env.JWT_SECRET);
+    } catch (error) {
+        
+    }
+}
