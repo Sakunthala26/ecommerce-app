ae7c2886cce71a131ba3f0d8ae09eec37f8bc8e5
Sakunthala26 <kumarisakunthala26@gmail.com>
2025-06-01 23:08:00 +0530

    new requst add

--- frontend/backend/controllers/productController.js
+++ frontend/backend/controllers/productController.js
@@ -50,7 +50,15 @@ try {
 
 //function for list product
 const listProduct = async (req,res) => {
+    try {
+        
+        const products = await productModel.find({});
+        res.json({success:true,products})
 
+    } catch (error) {
+        console.log(error)
+    res.json({success:false,message:error.message})
+    }
     
 }
 
@@ -58,12 +66,34 @@ const listProduct = async (req,res) => {
 //function for removing product
 const removeProduct = async (req,res) => {
 
+    try {
+        
+        await productModel.findByIdAndDelete(req.body.id)
+        res.json({success:true,message:"Product Removed"})
+
+    } catch (error) {
+          console.log(error)
+    res.json({success:false,message:error.message})
+    
+    }
+
     
 }
 
 
 //function for single product info
 const singleProduct = async (req,res) => {
+    try {
+
+        const { productId } = req.body
+        const product = await productModel.findById(productId)
+        res.json({success:true,product})
+        
+    } catch (error) {
+         console.log(error)
+    res.json({success:false,message:error.message})
+        
+    }
 
     
 }
